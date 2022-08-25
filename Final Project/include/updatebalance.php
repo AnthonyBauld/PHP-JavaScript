27115b842e534374158479934c7f8e19f6f88e4a
Anthony Bauld <102757875+AnthonyBauld@users.noreply.github.com>
2022-08-25 11:24:26 -0400

    Create updatebalance.php

---
+++ Final Project/include/updatebalance.php
@@ -0,0 +1,27 @@
+<?php
+/**
+    * I, Anthony. Bauld, certify that this material is my original work.  No other person's work has been used without due acknowledgement.   
+    * December 10, 2021.
+    *
+    * This software connects the .php code to the datebase and updates the balance.
+*/
+
+// The include function is used to eveulate the file "connect.php.".
+include "connect.php";             
+// filter and validate the inputted bet amount and places it into a variable $bet.
+$bet = filter_input(INPUT_GET, 'bet', FILTER_VALIDATE_INT);                           
+// Creates the "sql" variable, which will insert the id, item, quantity, and done into the shopping_list database.
+$sql = "UPDATE coinflip SET balance=$bet";  
+// Prepares the statement for the above command.
+$stmt = $dbh->prepare($sql);                      
+// Creates a if statement that if $item or $quantity are equal to null execute the success statement.              
+if ($bet != null && $bet != null) {       
+    // Executes the sucess statement.       
+    $success = $stmt->execute();                                           
+}
+// If $bet are not equal to null, an else statement is generated.
+else {
+    // If the else statement is executed, it returns false.
+    return false;                                               
+}
+?>
